30560042a4e7bdd72b2e25fc519b6ed462072b85
K. Canamar | Remote Full Stack Software Engineer <99228411+kcanamar@users.noreply.github.com>
2023-03-06 17:08:33 -0800

    setup tailwind, and dependencies

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="grid h-screen place-contnet-center gap-3">
+        <div class="flex flex-col text-center">
+            <h1 class="text-5xl text-blue-500">Welcome to Funkie Coding</h1>
+            <h4 class="text-2xl text-black-500">With Kcanmar</h4>
+        </div>
+    </div>
+</template>
